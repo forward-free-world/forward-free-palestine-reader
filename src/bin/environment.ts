3508610333ee18909b5e9c4a86f2f973b73fcb98
Theunis <dev@theuniscilliers.com>
2024-12-26 23:02:11 +0200

    Remove environment file

--- src/bin/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  summariseApi: 'https://tldrthis.p.rapidapi.com/v1/model/abstractive/summarize-url/',
-  summariseApiKey: 'ca7a4dec6amsh39e0ebaf793004cp134dbajsn62d99a061ebb'
-};
