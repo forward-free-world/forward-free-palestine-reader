de282e7826551e468a692a250a880b69d954c041
Theunis <dev@theuniscilliers.com>
2024-12-26 23:24:11 +0200

    Fix linux newline

--- src/utilities/regex.ts
+++ src/utilities/regex.ts
@@ -1,5 +1,5 @@
 export const ImageRegex = new RegExp('\\[\\/\\/\\]:#\\s*\\(Image:\\s*(.+)\\)');
 export const LinkRegex = new RegExp('\\[\\/\\/\\]:#\\s*\\(Link:\\s*(.+)\\)');
-export const MetaDataRegex = new RegExp(/^---\r\n\[\/\/\]:#[\s\S]+---/);
+export const MetaDataRegex = new RegExp(/^---\n\[\/\/\]:#[\s\S]+---/);
 export const TagsRegex = new RegExp('\\[\\/\\/\\]:#\\s*\\(Tags:\\s*(.+)\\)');
 export const TitleRegex = new RegExp('\\[\\/\\/\\]:#\\s*\\(Title:\\s*(.+)\\)');
